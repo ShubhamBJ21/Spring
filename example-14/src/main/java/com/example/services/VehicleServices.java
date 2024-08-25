7cbc6d340d6154bce44868de2ba2b8d1112c471d
Shubham Jadhav <shubhamjadhav21022002@gmail.com>
2024-08-25 19:17:54 +0530

    Singleton Bean Scope inside spring

--- example-14/src/main/java/com/example/services/VehicleServices.java
+++ example-14/src/main/java/com/example/services/VehicleServices.java
@@ -3,9 +3,12 @@ package com.example.services;
 import com.example.interfaces.Speakers;
 import com.example.interfaces.Tyres;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(BeanDefinition.SCOPE_SINGLETON)
 public class VehicleServices {
 
     @Autowired
