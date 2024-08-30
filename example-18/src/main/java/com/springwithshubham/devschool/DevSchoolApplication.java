3be0a50d654234ae5b1491eaf01f7a3007eeb1d8
Shubham Jadhav <shubhamjadhav21022002@gmail.com>
2024-08-30 12:10:56 +0530

    Hello World Web App using Spring Boot & Spring MVC

---
+++ example-18/src/main/java/com/springwithshubham/devschool/DevSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.springwithshubham.devschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevSchoolApplication.class, args);
+	}
+
+}
---
+++ example-18/src/test/java/com/springwithshubham/devschool/DevSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springwithshubham.devschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
