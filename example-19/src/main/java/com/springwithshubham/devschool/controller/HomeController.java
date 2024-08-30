ddd902a02afc76151d15ff61cebcfa2b3c5850bb
Shubham Jadhav <shubhamjadhav21022002@gmail.com>
2024-08-30 13:04:01 +0530

    Hello World Web App Using Thymeleaf & DevTools Demo

---
+++ example-19/src/main/java/com/springwithshubham/devschool/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.springwithshubham.devschool.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value={"", "/", "home"})
+    public String displayHomePage(Model model){
+        model.addAttribute("username", "Shubham Jadhav");
+        return "home.html";
+    }
+}
